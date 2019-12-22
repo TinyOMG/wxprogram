9670873275bfcd668949ebfa071c09b8703f60ba
jyz <1024618602@qq.com>
2019-12-22 18:23:16 +0800

    jyz

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,11 +1,17 @@
 package controller;
 
 
+import entity.Wxuser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import serviceimpl.WxuserServiceImpl;
 
 @RestController
 public class MyController {
+
+    @Autowired
+    WxuserServiceImpl wsi;
 
 @RequestMapping(value="/my",produces = "application/json;charset=utf-8")
     public String my(){
@@ -13,7 +19,18 @@ public class MyController {
     return "江雨哲";
 }
 
+    @RequestMapping(value="/insertSelective",produces = "application/json;charset=utf-8")
 
+    public int insertSelective(){
+
+        Wxuser wxuser=new Wxuser();
+
+        wxuser.setOpenid("123");
+        return wsi.insertSelective(wxuser);
+
+
+
+    }
 
 
 }
